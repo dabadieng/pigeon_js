47e8570ea5449756140255837076e429e3cd2a61
daba <dieng.daba@outlook.com>
2020-01-13 16:47:22 +0100

    save one

---
+++ code.js
@@ -0,0 +1,59 @@
+let pigeon=document.createElement("img");
+pigeon.src="pigeon.gif";
+pigeon.style.width="50px";
+pigeon.style.height="50px";
+pigeon.style.position="absolute";
+pigeon.style.top="0px";
+pigeon.style.left="0px";
+document.body.appendChild(pigeon);
+pigeon.dataset.vx=mtrand(-10,10);
+pigeon.dataset.vy=mtrand(-10,10);
+pigeon.dataset.etat="vivant";
+let timer=setInterval(deplacer,40);
+pigeon.addEventListener("click",kill);
+
+//gestion de la souris
+window.addEventListener("mousemove",calculdistance);
+
+function calculdistance(event) {
+    let x=event.clientX;
+    let y=event.clientY;
+    let a=parseInt(pigeon.style.left);
+    let b=parseInt(pigeon.style.top);
+    let d=parseInt(Math.sqrt((x-a)*(x-a) + (y-b)*(y-b)));
+    console.log(d);
+}
+
+
+function kill() {
+    pigeon.src="pigeondead.jpg";
+    pigeon.dataset.etat="mort";
+    pigeon.dataset.vx=0;
+    pigeon.dataset.vy=5;
+}
+
+function deplacer() {
+    let x=parseInt(pigeon.style.left);
+    let y=parseInt(pigeon.style.top);
+    x += parseInt(pigeon.dataset.vx);
+    y += parseInt(pigeon.dataset.vy);
+    if (x>window.innerWidth)
+        pigeon.dataset.vx=-pigeon.dataset.vx;
+    else if (x<0)
+        pigeon.dataset.vx=-pigeon.dataset.vx;
+    
+    if (y>window.innerHeight) {
+        if (pigeon.dataset.etat=="mort")
+            pigeon.dataset.vy=0;        
+        pigeon.dataset.vy=-pigeon.dataset.vy;
+    } else if (y<0)
+        pigeon.dataset.vy=-pigeon.dataset.vy;
+
+    pigeon.style.left=x + "px";
+    pigeon.style.top=y + "px";
+}
+
+//Tire au hasard un nombre entre para1 et para2
+function mtrand(para1,para2) {
+    return (para2-para1)*Math.random() + para1;
+}
